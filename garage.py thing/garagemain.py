2d863e181b3a77e60e05f6902b8ed6757755df90
eladgura <Eladgura2@gmail.com>
2024-05-26 19:21:15 +0300

    first commit

---
+++ garage.py thing/garagemain.py
@@ -0,0 +1,31 @@
+from garagebranch import add_car, del_car, print_car_info, update_car, clear_screen, save_car_info_to_json, load_car_info_from_json
+
+car_info = load_car_info_from_json()
+
+def display_menu():
+    while True:
+        clear_screen()
+        print("1 - Exit")
+        print("2 - Add car")
+        print("3 - Update car")
+        print("4 - Delete car")
+        print("5 - Print all cars")
+
+        user_selection = input("What is your selection? ")
+        if user_selection == "1":
+            save_car_info_to_json(car_info)
+            exit()
+        elif user_selection == "2":
+            add_car(car_info)
+        elif user_selection == "3":
+            update_car(car_info)
+        elif user_selection == "4":
+            del_car(car_info)
+        elif user_selection == "5":
+            print_car_info(car_info)
+        else:
+            print("Please choose a valid option.")
+
+if __name__ == "__main__":
+    clear_screen()
+    display_menu()
